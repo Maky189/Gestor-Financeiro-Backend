51053226ecc9483ded50a491e3e5c70d460b59bd
maky189 <maky188pgt555@gmail.com>
2025-12-03 11:40:12 -0100

    minor fixes to database

--- sql/Db.sql
+++ sql/Db.sql
@@ -3,53 +3,25 @@ USE gestor_db;
 
 CREATE TABLE utilizador(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    data_nascimento DATE,
-    email VARCHAR(100),
+    nome VARCHAR(255) NOT NULL,
+    apelido VARCHAR(255)NOT NULL,
+    email VARCHAR(255)NOT NULL,
+    password VARCHAR(255)NOT NULL,
     hora_de_registo TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 CREATE TABLE categorias(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome ENUM("Gastos ","Rendimento ", "Entretenimento ", ),
-)
-
-CREATE TABLE conta(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    numero_conta VARCHAR(30) UNIQUE,
-    credito DOUBLE,
-    debito DOUBLE,
-    emprestimos DOUBLE,
-    data DATE DEFAULT (CURRENT_DATE),
+    id INT PRIMARY KEY AUTO_INCREMENT,  
+    nome VARCHAR(30) NOT NULL,
     utilizador_id INT,
     FOREIGN KEY (utilizador_id) REFERENCES utilizador(id)
 );
 
-DELIMITER $$
-
-CREATE TRIGGER gerar_numero_conta
-BEFORE INSERT ON conta
-FOR EACH ROW 
-BEGIN
-    SET NEW.numero_conta = CONCAT(
-        "CV",
-        LPAD(FLOOR(RAND()*999999999999999), 16, "0") -- gera um numero aleatorio para o numero de conta
-    );
-END $$
-
-DELIMITER ;
-
-CREATE TABLE transacao(
+CREATE TABLE conta(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    valor DOUBLE,
-    tipo ENUM("Crédito","Débito") NOT NULL, -- define o tipo da transação
+    numero_conta VARCHAR(30) UNIQUE,
+    saldo_atual DOUBLE,
+    emprestimos DOUBLE,
     data DATE DEFAULT (CURRENT_DATE),
-    conta_id INT,
-    FOREIGN KEY (conta_id) REFERENCES conta(id)
-);
-
-CREATE TABLE saldo_total(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    data DATE NOT NULL,
     utilizador_id INT,
     FOREIGN KEY (utilizador_id) REFERENCES utilizador(id)
 );
