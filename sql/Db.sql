a6675a09b895fd76d740f23cc78877d57cc9b716
B1mb4z <elvin007pires@gmail.com>
2025-12-05 18:11:21 -0100

    melhoria do code

--- sql/Db.sql
+++ sql/Db.sql
@@ -5,11 +5,12 @@ CREATE TABLE utilizador(
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
-    apelido VARCHAR(255)NOT NULL,
-    email VARCHAR(255)NOT NULL,
-    password VARCHAR(255)NOT NULL,
+    apelido VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     hora_de_registo TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 CREATE TABLE categorias(
     id INT PRIMARY KEY AUTO_INCREMENT,  
     nome VARCHAR(30) NOT NULL,
@@ -26,9 +27,13 @@ CREATE TABLE conta(
     utilizador_id INT,
     FOREIGN KEY (utilizador_id) REFERENCES utilizador(id)
 );
+
 CREATE TABLE gastos(
     id INT PRIMARY KEY AUTO_INCREMENT,
+    descricao VARCHAR(255) NOT NULL,
     gasto VARCHAR(30) NOT NULL,
     preco DOUBLE NOT NULL,
-    data DATE DEFAULT (CURRENT_DATE)
-)
+    data DATE DEFAULT (CURRENT_DATE),
+    categoria_id INT,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+);
