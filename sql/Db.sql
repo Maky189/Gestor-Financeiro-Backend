11bf8779ff59d8db83ea58ab00d46b7080788939
B1mb4z <elvin007pires@gmail.com>
2025-12-08 17:50:38 -0100

    atualizacao

--- sql/Db.sql
+++ sql/Db.sql
@@ -4,11 +4,13 @@ USE gestor_db;
 CREATE TABLE utilizador(
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
-    apelido VARCHAR(255)NOT NULL,
-    email VARCHAR(255)NOT NULL,
-    password VARCHAR(255)NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    apelido VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     hora_de_registo TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
 CREATE TABLE categorias(
     id INT PRIMARY KEY AUTO_INCREMENT,  
     nome VARCHAR(30) NOT NULL,
@@ -28,7 +30,21 @@ CREATE TABLE conta(
 
 CREATE TABLE gastos(
     id INT PRIMARY KEY AUTO_INCREMENT,
+    descricao VARCHAR(255) NOT NULL,
     gasto VARCHAR(30) NOT NULL,
     preco DOUBLE NOT NULL,
-    data DATE DEFAULT (CURRENT_DATE)
-)
+    data DATE DEFAULT (CURRENT_DATE),
+    categoria_id INT,
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+);
+
+CREATE TABLE chaves(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    chave VARCHAR(255) NOT NULL,
+    gasto_id INT NOT NULL,
+    FOREIGN KEY (gasto_id) REFERENCES gastos(id),
+    conta_id INT NOT NOT,
+    FOREIGN KEY (conta_id) REFERENCES conta(id),
+    categoria_id INT NOT NULL,
+    FOREIGN KEY (categoria_id) REFERENCES (categorias)
+);
