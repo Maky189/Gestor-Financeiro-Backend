3cbea9afd3f9f558e7cc46c67edff96b6c495615
B1mb4z <elvin007pires@gmail.com>
2025-12-11 17:57:26 -0100

    Alteracoes no campo utilizador

--- sql/Db.sql
+++ sql/Db.sql
@@ -1,58 +1,65 @@
 CREATE DATABASE gestor_db;
 USE gestor_db;
 
+-- Tabela de utilizadores
 CREATE TABLE utilizador(
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
-    apelido VARCHAR(255) NOT NULL,
+    endereco VARCHAR(255) NOT NULL,
+    telemovel VARCHAR(20) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     data_de_registo TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE criar_categorias(
+-- Tabela de categorias
+CREATE TABLE categorias(
     id INT PRIMARY KEY AUTO_INCREMENT,  
     nome VARCHAR(30) NOT NULL,
     utilizador_id INT,
     FOREIGN KEY (utilizador_id) REFERENCES utilizador(id)
 );
 
+-- Tabela de contas
 CREATE TABLE conta(
     id INT PRIMARY KEY AUTO_INCREMENT,
     numero_conta VARCHAR(30) UNIQUE,
     saldo_atual DOUBLE,
     emprestimos DOUBLE,
-    data DATE DEFAULT (CURRENT_DATE),
+    data TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     utilizador_id INT,
     FOREIGN KEY (utilizador_id) REFERENCES utilizador(id)
 );
 
+-- Tabela de gastos
 CREATE TABLE gastos(
     id INT PRIMARY KEY AUTO_INCREMENT,
     descricao VARCHAR(255) NOT NULL,
     gasto VARCHAR(30) NOT NULL,
     preco DOUBLE NOT NULL,
-    data DATE DEFAULT (CURRENT_DATE),
+    data TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     categoria_id INT,
-    FOREIGN KEY (categoria_id) REFERENCES criar_categorias(id)
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
 );
 
+-- Tabela de chaves
 CREATE TABLE chaves(
     id INT PRIMARY KEY AUTO_INCREMENT,
     chave VARCHAR(255) NOT NULL,
     gasto_id INT NOT NULL,
-    FOREIGN KEY (gasto_id) REFERENCES gastos(id),
     conta_id INT NOT NULL,
-    FOREIGN KEY (conta_id) REFERENCES conta(id),
     categoria_id INT NOT NULL,
-    FOREIGN KEY (categoria_id) REFERENCES criar_categorias(id)
+    FOREIGN KEY (gasto_id) REFERENCES gastos(id),
+    FOREIGN KEY (conta_id) REFERENCES conta(id),
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
 );
 
-CREATE TABLE nome_categoria(
+-- Tabela de produtos por categoria
+CREATE TABLE produtos(
     id INT PRIMARY KEY AUTO_INCREMENT,
     categoria_id INT NOT NULL,
     produto VARCHAR(255) NOT NULL,
     valor DOUBLE NOT NULL,
-    FOREIGN KEY (categoria_id) REFERENCES criar_categorias(id)
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
 );
